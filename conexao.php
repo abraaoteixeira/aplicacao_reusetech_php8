600b53e496e2255bf371a29dd65de477dd4ad461
anthonyvmuller <anthonyvmuller@outlook.com>
2021-02-15 19:08:27 -0300

    Automatizando as adições

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
     $servidor = "localhost";
     $usuario = "root";
-    $senha = "root1234";
+    $senha = "anthonypro";
     $banco = "dbreusetech";
     $conecta = mysqli_connect($servidor, $usuario, $senha, $banco);
     if(mysqli_connect_errno($conecta)){
