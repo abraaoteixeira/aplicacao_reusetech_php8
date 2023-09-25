099314cce9fe59ad5667f429c7866f2e5c151626
abraaoteixeira <abraaoteixeira0101@gmail.com>
2023-09-25 14:42:59 -0300

    teste e testes

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 session_start();
@@ -16,10 +15,82 @@ if(!isset($_SESSION["login"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplicativo ReuseTech</title>
-    <!--LINKS CSS-->
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/responsive.css">
+
+    <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js"></script>
+
+    <style>
+        body {
+            background-color: #ffffff;
+            color: #000000;
+            font-family: Arial, sans-serif;
+            margin: 0; /* Remove a margem padrão do corpo */
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh; /* Garante que o corpo ocupa toda a altura da tela */
+        }
+
+        header {
+            background-color: #000000;
+            padding: 10px;
+            text-align: center;
+            border-radius: 10px 10px 0 0;
+            flex-shrink: 0; /* Não permite que o cabeçalho encolha */
+        }
+
+        header h1 {
+            color: #00ff00;
+        }
+
+        main {
+            padding: 20px;
+            border-radius: 0 0 10px 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #f7f7f7;
+            flex-grow: 1; /* Permite que o conteúdo principal cresça e ocupe o espaço disponível */
+        }
+
+        .conteudo {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .rodape {
+            background-color: #000000;
+            padding: 10px;
+            border-radius: 0 0 10px 10px;
+            text-align: center;
+            color: #00ff00;
+            margin-top: auto; /* Mantém o rodapé na parte inferior */
+        }
+
+        /* Botões */
+        #funcoes a {
+            background-color: transparent; /* Sem cor de fundo */
+            color: #000; /* Texto preto */
+            padding: 8px 16px; /* Reduzi o padding para torná-los um pouco menores */
+            border-radius: 5px;
+            text-decoration: none;
+            display: inline-block;
+            margin: 10px 5px; /* Adaptei a margem para manter a distância entre os botões */
+            font-size: 16px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            transition: background-color 0.3s ease;
+        }
+
+        #funcoes a:hover {
+            background-color: bl;
+        }
+
+    </style>
 </head>
 <body>
     <header>
@@ -27,21 +98,45 @@ if(!isset($_SESSION["login"])){
         <h1>ReuseTech</h1>
     </header>
     <main>
-        <div id="funcoes">
-            <a href="visualizar.php">
-                <h1>Visualizar Estoque</h1>
-                <p>Click aqui para visualizar o nosso estoque de peças</p>
-            </a>
-        </div>
-        <div id="funcoes">
-            <a href="adicionar.php">
-                <h1>Adicionar Peça do Estoque</h1>
-                <p>Click aqui para adicionar peças em nosso estoque</p>
-            </a>
-        </div>
-        <div class="sair">
-            <a href="sair.php"><input type="button" value="Sair"></a>
+        <div class="conteudo">
+            <!-- Conteúdo principal aqui -->
+            <div id="funcoes">
+                <a href="visualizar.php">
+                    <h1>Visualizar Estoque</h1>
+                    <p>Clique aqui para visualizar o nosso estoque de peças</p>
+                </a>
+                <a href="adicionar.php">
+                    <h1>Adicionar Peça ao Estoque</h1>
+                    <p>Clique aqui para adicionar peças ao nosso estoque</p>
+                </a>
+            </div>
         </div>
     </main>
+    <div class="rodape">
+        &copy; 2023 ReuseTech. Todos os direitos reservados.
+    </div>
+    <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAW8Nu8CnS0rQyjH1r42r2wQYKW4Qr4eE4",
+    authDomain: "reusetech-oficial.firebaseapp.com",
+    projectId: "reusetech-oficial",
+    storageBucket: "reusetech-oficial.appspot.com",
+    messagingSenderId: "227940630480",
+    appId: "1:227940630480:web:8b34435375efbccfea03d5",
+    measurementId: "G-D1VGWLW5S4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 </body>
 </html>
--- pecas/adicionar_peca.php
+++ pecas/adicionar_peca.php
@@ -1,74 +1,146 @@
-<?php
-    require_once("../conexao.php");
-
-    if (isset($_POST['table'])){
-        $table = $_POST['table'];
-    }
-    if (isset($_GET['table'])){
-        $table = $_GET['table'];
-    }
-
-    if(isset($_POST["marca"])){
-        $query = mysqli_query($conecta, "SHOW COLUMNS FROM $table");
-        $columns = $atributes = '';
-
-        while($row = mysqli_fetch_assoc($query)){
-            if($row['Field'] != "id"){
-                $columns .= "{$row['Field']}";
-                $atributes .= "'{$_POST[$row['Field']]}', ";
-            }
-        }
-
-        $columns = rtrim($columns, ', ');
-        $atributes = rtrim($atributes, ', ');
-
-        $inserir = "INSERT INTO $table($columns) VALUES ($atributes)";
-        $insert = mysqli_query($conecta, $inserir);
-
-        if(!$insert){
-            die("Erro no Banco de Dados");
-        } else {
-            header("location:../certo.html");
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Placa Mãe - ReuseTech</title>
-        <link rel="stylesheet" href="../css/main.css">
-        <link rel="stylesheet" href="../css/header.css">
-        <link rel="stylesheet" href="../css/form.css">
-        <link rel="stylesheet" href="../css/pecas.css">
-        <link rel="stylesheet" href="../css/responsive.css">
-    </head>
-    <body>
-        <header>
-            <img src="../images/reusetech.png" alt="ReuseTech">
-            <h1>ReuseTech</h1>
-        </header>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Peça - ReuseTech</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../css/pecas.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <style>
+        /* Gradiente de fundo */
+        body {
+        background: linear-gradient(to bottom, #ffffff, #f0f0f0);
+        }
+
+        /* Caixa de formulário estilizada */
+        form {
+        background-color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        max-width: 400px;
+        margin: 0 auto;
+        }
+
+        label {
+        font-weight: bold;
+        }
+
+        input[type="text"],
+        input[type="number"] {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        }
+
+        input[type="submit"] {
+        background-color: #ff5733;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+        }
+
+        input[type="submit"]:hover {
+        background-color: #e5472b;
+        }
+
+        /* Alinhar o botão de voltar */
+        .sair {
+        text-align: center;
+        }
+
+        .sair input[type="button"] {
+        background-color: #333;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+        }
+
+        .sair input[type="button"]:hover {
+        background-color: #555;
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <img src="../images/reusetech.png" alt="ReuseTech">
+        <h1>ReuseTech</h1>
+    </header>
+    <div class="form-container">
         <form action="adicionar_peca.php" method="POST">
             <?php
-                $query = mysqli_query($conecta, "SHOW COLUMNS FROM $table");
+                require_once("../conexao.php");
 
-                echo "<input type='hidden' value='$table' name='table' >";
-                while($row = mysqli_fetch_assoc($query)){
-                    if($row['Field'] != "id"){
-                        $inputType = (strpos($row['Type'], "int") !== false) ? "number" : "text";
-                        echo "<label>{$row['Field']} {$row['Type']}</label>";
-                        echo "<input type='$inputType' placeholder='{$row['Field']}' name='{$row['Field']}'><br>";
+                // Verifique se a variável $table está definida e não vazia
+                $table = $_POST['table'] ?? $_GET['table'] ?? null;
+
+                if (empty($table)) {
+                    die("Erro: Tabela não especificada.");
+                }
+
+                if(isset($_POST["marca"])){
+                    $query = mysqli_query($conecta, "SHOW COLUMNS FROM " . mysqli_real_escape_string($conecta, $table));
+                    $columns = $atributes = '';
+
+                    while($row = mysqli_fetch_assoc($query)){
+                        if($row['Field'] !== "id"){
+                            $columns .= "{$row['Field']}";
+                            $atributes .= "'" . mysqli_real_escape_string($conecta, $_POST[$row['Field']]) . "', ";
+                        }
+                    }
+
+                    $columns = rtrim($columns, ', ');
+                    $atributes = rtrim($atributes, ', ');
+
+                    $inserir = "INSERT INTO " . mysqli_real_escape_string($conecta, $table) . "($columns) VALUES ($atributes)";
+                    $insert = mysqli_query($conecta, $inserir);
+
+                    if(!$insert){
+                        die("Erro no Banco de Dados: " . mysqli_error($conecta));
+                    } else {
+                        header("location:../certo.html");
+                        exit();
                     }
                 }
-                echo '<input type="submit" value="Adicionar">';
             ?>
+
+            <!-- Seu código PHP para gerar os campos -->
+            <?php
+                $query = mysqli_query($conecta, "SHOW COLUMNS FROM " . mysqli_real_escape_string($conecta, $table));
+                echo "<input type='hidden' value='$table' name='table' >";
+                while($row = mysqli_fetch_assoc($query)){
+                    if($row['Field'] !== "id"){
+                        $inputType = (strpos($row['Type'], "int") !== false) ? "number" : "text";
+                        echo "<label>{$row['Field']} {$row['Type']}</label>";
+                        echo "<input type='$inputType' placeholder='{$row['Field']}' name='{$row['Field']}' required><br>"; // Adicionei 'required' para tornar os campos obrigatórios
+                    }
+                }
+            ?>
+            <!-- Fim do código PHP -->
+            
+            <input type="submit" value="Adicionar">
         </form>
-        <div class="sair">
-            <a href="../adicionar.php"><input type="button" value="Voltar"></a>
-        </div>
-    </body>
+    </div>
+    <div class="sair">
+        <a href="../adicionar.php"><input type="button" value="Voltar"></a>
+    </div>
+</body>
 </html>
 
 <?php
